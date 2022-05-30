7576e8907ac63da1dbcb21047e14a6ac990d591d
ermz <eramirez4369@gmail.com>
2022-05-30 19:07:10 -0400

    Added additional aave interfaces and worked on aave_borrow script

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 1. Swap our ETH for WETH
-2. Deposit some ETH into AAVE
+2. Deposit some ETH (WETH) into AAVE
 3. Borrow some asset with ETH collateral
     1. Sell that borrowed asset. (Short selling)
 4. Repay everything back
--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -11,6 +11,7 @@ def get_weth():
     account = get_account()
     weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
     tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
+    tx.wait(1)
     print("Received 0.1 WETH")
     return tx
 
